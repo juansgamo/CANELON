e4069474b883c5dae9a29af9fa99eafd4a014b3b
Regem27 <mario.reyes@alumnos.upm.es>
2024-04-19 11:44:15 +0200

    Prueba de menu principal3

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,8 +3,14 @@ extends Control
 func _ready():
 	$menufondo.play()
 
-func _on_iniciar_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
-
 func _on_salir_pressed():
 	get_tree().quit()
+
+func _on_ajustes_pressed():
+	get_tree().change_scene_to_file("res://ajustes.tscn")
+
+func _on_modo_individual_pressed():
+	pass # Replace with function body.
+
+func _on_modo_multijugador_pressed():
+	pass # Replace with function body.
