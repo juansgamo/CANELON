59cec96fa7f4f0d64253312e1eac74cf008450b1
Regem27 <mario.reyes@alumnos.upm.es>
2024-04-19 12:37:14 +0200

    Menu de ajustes

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,7 @@ func _on_salir_pressed():
 	get_tree().quit()
 
 func _on_ajustes_pressed():
-	get_tree().change_scene_to_file("res://ajustes.tscn")
+	$Ajustes.popup() 
 
 func _on_modo_individual_pressed():
 	pass # Replace with function body.
